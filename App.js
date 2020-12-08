045462fb3b9ac7c2337715609e15e518a954b68c
Guan Xian <gxleow@gmail.com>
2020-12-08 11:35:41 +0800

    button

--- App.js
+++ App.js
@@ -1,38 +1,74 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
+import { FlatList } from "react-native-gesture-handler";
 
 function HomeScreen() {
+  const [colorArray, setColorArray] = useState([]);
+
+  function renderItem({ item }) {
+    return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+  }
+
+  function addColor() {
+    let newColor = {
+      red: Math.floor(Math.random() * 256),
+      green: Math.floor(Math.random() * 256),
+      blue: Math.floor(Math.random() * 256),
+      id: colorArray.length.toString(),
+    };
+    setColorArray([...colorArray, newColor]);
+  }
+
+  function resetColor() {
+    setColorArray([]);
+  }
+
+
   return (
     <View style={styles.container}>
-      <BlockRGB red={200} green={0} blue={0} />
-      <BlockRGB red={0} green={190} blue={0} />
-      <BlockRGB red={0} green={0} blue={150} />
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "blue" }}>Add Colour</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={resetColor}
+      >
+        <Text style={{ color: "red" }}>Reset Colour</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        style={{ width: "100%" }}
+        data={colorArray}
+        renderItem={renderItem}
+      />
     </View>
   );
-  }
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Colours" component={HomeScreen} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Colour Bar" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#fff",
-   alignItems: "center",
-   justifyContent: "center",
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
 });
-
